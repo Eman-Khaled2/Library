c73e6a616eb1d6f9ccbdfeb616ba09b2ff1d7ccb
Eman Khaled <eman42kh@gmail.com>
2024-05-30 22:28:26 +0300

    new edit

---
+++ src/myP/date1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package myP;
+
+/**
+ *
+ * @author asus
+ */
+class date1 {
+
+    static Object getDateEditor() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
